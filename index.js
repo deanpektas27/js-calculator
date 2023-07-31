1f0f0f82cbbce3ec5335c75a66378eeb1e42fcd5
Dean Pektas <deanpektas12345@gmail.com>
2023-07-30 20:27:49 -0400

    Completed basic operations with two numbers only

--- index.js
+++ index.js
@@ -1,31 +1,96 @@
-function add() {
-
-}
-
-function subtract() {
-
-}
-
-function multiply() {
-
-}
-
-function divide() {
-
-}
-
-function operate(operator, num1, num2) {
-    if(operator === "+") add(num1, num2);
-    else if(operator === "-") subtract(num1, num2);
-    else if(operator === "*") multiply(num1, num2);
-    else if(operator === "/") divide(num1, num2);
-}
-
 const calcDisplay = document.querySelector('.calcDisplay');
 calcDisplay.innerText = "123456";
-let displayValue = calcDisplay.textContent;
+let displayValue;
+let num1 = 0;
+let num2 = 0;
+let operator = '';
+let result = 0;
 
-const btn = document.querySelectorAll('#btn');
-btn.forEach(btn => btn.onclick = () => {
-    alert('click!');
+const numberButton = document.querySelectorAll('#numBtn');
+const operatorButton = document.querySelectorAll('#opbtn');
+const clearButton = document.querySelector('#clrbtn');
+clearButton.onclick = () => clrButton();
+
+
+// Operations to perform after numbers are entered
+function add(num1, num2) {
+    result = num1 + num2;
+    calcDisplay.innerText = result;
+}
+
+function subtract(num1, num2) {
+    result = num1 - num2;
+    calcDisplay.innerText = result;
+}
+
+function multiply(num1, num2) {
+    result = num1 * num2;
+    calcDisplay.innerText = result;
+}
+
+function divide(num1, num2) {
+    result = num1 / num2;
+    calcDisplay.innerText = result;
+}
+
+///////////////////////////////
+
+// Decide what to do with the given numbers
+function operate(operator, num1, num2) {
+    console.log(`deciding operation!!! I have ${operator}`);
+    if(operator == '+') {
+        add(num1, num2);
+    } else if(operator === "-") {
+        subtract(num1, num2);
+    } else if(operator === "*") {
+        multiply(num1, num2);
+    } else if(operator === "/") {
+        divide(num1, num2);
+    }
+}
+
+// Identifies button pressed is a number
+function numButton(number) {
+    if(calcDisplay.innerText == '0') calcDisplay.innerText = '';
+    calcDisplay.innerText += number;
+}
+
+// Identifies button pressed is an operator
+function opButton(op) {
+    // If an operator btn (not =) is pushed,
+    // store number in variables
+    if(op != '=') {
+        operator = op;
+        num1 = Number(calcDisplay.innerText);
+        console.log(`First number entered is ${num1}`);
+        calcDisplay.innerText = 0;
+    } else if(op == '=') {
+        if(num2 == 0) {
+            num2 = Number(calcDisplay.innerText);
+            console.log(`Second number entered is ${num2}`);
+            calcDisplay.innerText = 0;
+        }
+        operate(operator, num1, num2);
+    }
+}
+
+function clrButton() {
+    console.log('Clear!');
+    calcDisplay.innerText = 0;
+    num1 = 0;
+    num2 = 0;
+    operator = '';
+    result = 0;
+}
+
+numberButton.forEach(btn => btn.onclick = () => {
+    numButton(btn.textContent);
+    displayValue = calcDisplay.innerText;
+    console.log(displayValue);
+});
+
+operatorButton.forEach(btn => btn.onclick = () => {
+    opButton(btn.textContent);
+    displayValue = calcDisplay.innerText;
+    console.log(displayValue);
 });
